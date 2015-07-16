d47dc75ebf7d094055892fdc007563f200f241cd
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-07-15 22:40:09 -0400

    Initial commit for Swagger
    The swagger ui can be accessed from:
    http://localhost:8080/reports-rest/swagger-ui/
    
    Due to an issue with Swagger-Resteasy library not being able to read
    JAXRS annotations from interfaces, we decided to remove the interface
    and add those JAXRS annotations to the implemented classes instead. As
    such, `Artifacts.java` is switched from an interface to a concrete
    class, and `ArtifactsImpl.java` is removed. Same thing for `Root.java`.
    
    The `pom.xml` is modified to add the swagger dependency.
    
    The `web.xml` is modified to add the servlet that will serve the REST
    documentation JSON data at
    http://localhost:8080/reports-rest/rest/api-docs
    
    The `webapp` folder now includes another folder, `swagger-ui`, that will
    consume the documentation JSON data and provides a nice webview of the
    REST endpoints.
    
    Note that we are using v2.0.24 for `swagger-ui`, since the latest one
    from master can't properly display the top-level list of apis available
    right now.

--- reports-rest/src/main/java/org/jboss/da/listings/rest/api/Artifacts.java
+++ reports-rest/src/main/java/org/jboss/da/listings/rest/api/Artifacts.java
@@ -1,7 +1,10 @@
 package org.jboss.da.listings.rest.api;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,63 +14,126 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.jboss.da.listings.api.service.BlackArtifactService;
+import org.jboss.da.listings.api.service.WhiteArtifactService;
+import org.jboss.da.listings.rest.RestConvert;
 import org.jboss.da.listings.rest.api.model.ContainsResponse;
 import org.jboss.da.listings.rest.api.model.RestArtifact;
 import org.jboss.da.listings.rest.api.model.SuccessResponse;
 
 /**
- * 
+ *
  * @author Jozef Mrazek <jmrazek@redhat.com>
  *
  */
 @Path("/listings")
-public interface Artifacts {
+@Api(value = "/listings", description = "Listings of black and white artifacts")
+public class Artifacts {
+
+    @Inject
+    private RestConvert convert;
+
+    @Inject
+    private WhiteArtifactService whiteService;
+
+    @Inject
+    private BlackArtifactService blackService;
 
     @POST
     @Path("/white")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    SuccessResponse addWhiteArtifact(RestArtifact artifact);
+    @ApiOperation(value="Add White Artifact")
+    public SuccessResponse addWhiteArtifact(RestArtifact artifact) {
+        SuccessResponse response = new SuccessResponse();
+        response.setSuccess(whiteService.addArtifact(artifact.getGroupId(),
+                artifact.getArtifactId(),
+                artifact.getVersion()));
+        return response;
+    }
 
     @POST
     @Path("/black")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    SuccessResponse addBlackArtifact(RestArtifact artifact);
+    @ApiOperation(value="Add Black Artifact")
+    public SuccessResponse addBlackArtifact(RestArtifact artifact) {
+        SuccessResponse response = new SuccessResponse();
+        response.setSuccess(blackService.addArtifact(artifact.getGroupId(),
+                artifact.getArtifactId(),
+                artifact.getVersion()));
+        return response;
+    }
 
     @GET
     @Path("/white")
     @Produces(MediaType.APPLICATION_JSON)
-    ContainsResponse isWhiteArtifactPresent(@QueryParam("groupid") String groupId,
-            @QueryParam("artifactid") String artifactId,
-            @QueryParam("version") String version);
+    @ApiOperation(value = "Test if artifact is a white artifact")
+    public ContainsResponse isWhiteArtifactPresent(@QueryParam("groupid") String groupId,
+                   @QueryParam("artifactid") String artifactId,
+                   @QueryParam("version") String version) {
+        ContainsResponse response = new ContainsResponse();
+        response.setContains(whiteService.isArtifactPresent(groupId, artifactId, version));
+        return response;
+    }
 
     @GET
     @Path("/black")
     @Produces(MediaType.APPLICATION_JSON)
-    ContainsResponse isBlackArtifactPresent(@QueryParam("groupid") String groupId,
-            @QueryParam("artifactid") String artifactId,
-            @QueryParam("version") String version);
+    @ApiOperation(value = "Test if artifact is a black artifact")
+    public ContainsResponse isBlackArtifactPresent(@QueryParam("groupid") String groupId,
+                   @QueryParam("artifactid") String artifactId,
+                   @QueryParam("version") String version) {
+        ContainsResponse response = new ContainsResponse();
+        response.setContains(blackService.isArtifactPresent(groupId, artifactId, version));
+        return response;
+    }
 
     @GET
     @Path("/whitelist")
     @Produces(MediaType.APPLICATION_JSON)
-    Collection<RestArtifact> getAllWhiteArtifacts();
+    @ApiOperation(value = "Get all the white artifacts")
+    public Collection<RestArtifact> getAllWhiteArtifacts() {
+        List<RestArtifact> artifacts = new ArrayList<RestArtifact>();
+        artifacts.addAll(convert.toRestArtifacts(whiteService.getAll()));
+        return artifacts;
+    }
 
     @GET
     @Path("/blacklist")
     @Produces(MediaType.APPLICATION_JSON)
-    Collection<RestArtifact> getAllBlackArtifacts();
-    
+    @ApiOperation(value = "Get all the black artifacts")
+    public Collection<RestArtifact> getAllBlackArtifacts() {
+        List<RestArtifact> artifacts = new ArrayList<RestArtifact>();
+        artifacts.addAll(convert.toRestArtifacts(blackService.getAll()));
+        return artifacts;
+    }
+
     @DELETE
     @Path("/white")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    SuccessResponse removeWhiteArtifact(RestArtifact artifact);
+    @ApiOperation(value = "Remove white artifact")
+    public SuccessResponse removeWhiteArtifact(RestArtifact artifact) {
+        SuccessResponse response = new SuccessResponse();
+        response.setSuccess(whiteService.removeArtifact(artifact.getGroupId(),
+                artifact.getArtifactId(),
+                artifact.getVersion()));
+        return response;
+    }
 
     @DELETE
     @Path("/black")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    SuccessResponse removeBlackArtifact(RestArtifact artifact);
+    @ApiOperation(value = "Remove black artifact")
+    public SuccessResponse removeBlackArtifact(RestArtifact artifact) {
+        SuccessResponse response = new SuccessResponse();
+        response.setSuccess(blackService.removeArtifact(artifact.getGroupId(),
+                artifact.getArtifactId(),
+                artifact.getVersion()));
+        return response;
+    }
 }
--- reports-rest/src/main/java/org/jboss/da/listings/rest/api/Root.java
+++ reports-rest/src/main/java/org/jboss/da/listings/rest/api/Root.java
@@ -4,16 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 /**
- * 
+ *
  * @author Jozef Mrazek <jmrazek@redhat.com>
  *
  */
 @Path("/")
-public interface Root {
+public class Root {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    String getDescription();
+    public String getDescription() {
+        return "<h1>Dependency analyzer REST</h1>" + "<ul><li><strong>Version:</strong> 1</li>"
+                + "<li><strong>Documentation:</strong> https://docs.engineering.redhat.com/display/JP/REST+endpoints+proposal</li></ul>";
+    }
+
 }
