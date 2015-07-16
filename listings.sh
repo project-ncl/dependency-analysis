c95f49d4c175cf182b0e84dd19836e58d48df134
Honza Brázdil <jbrazdil@redhat.com>
2015-07-16 14:34:48 +0200

    [DA-60] Split B&W list CLI into two with different permissions

--- listings.sh
+++ listings.sh
@@ -2,10 +2,13 @@
 
 target=10.3.10.85:8080/reports-rest/rest/listings
 
-printUsage() {
-    echo "$0 (add|delete|check) (b[lack]|w[hite]) GROUP_ID:ARTIFACT_ID:VERSION"
-    echo "$0 list (b[lack]|w[hite])"
-    exit
+setColor() {
+    case $1 in
+        w) color=white ;;
+        b) color=black ;;
+        white|black) color=$1;;
+        *) printUsage; exit ;;
+    esac
 }
 
 pretyprintGAV() {
@@ -19,6 +22,7 @@ pretyprintGAV() {
 }
 
 list() {
+    setColor $1
     curl -s -H "Content-Type: application/json" -X GET $target/${color}list | pretyprintGAV
 }
 
@@ -34,7 +38,8 @@ matchGAV() {
 }
 
 delete() {
-    matchGAV $1
+    setColor $1
+    matchGAV $2
     tmpfile=`mktemp`
     curl -s -H "Content-Type: application/json" -X DELETE -d '{"groupId":"'${groupId}'", "artifactId":"'${artifactId}'", "version":"'${version}'"}' "$target/$color" > $tmpfile
     if ! grep -q '"success":true' $tmpfile; then
@@ -46,7 +51,8 @@ delete() {
 }
 
 add() {
-    matchGAV $1
+    setColor $1
+    matchGAV $2
     tmpfile=`mktemp`
     curl -s -H "Content-Type: application/json" -X POST -d '{"groupId":"'${groupId}'", "artifactId":"'${artifactId}'", "version":"'${version}'"}' "$target/$color" > $tmpfile
     if ! grep -q '"success":true' $tmpfile; then
@@ -58,7 +64,8 @@ add() {
 }
 
 check() {
-    matchGAV $1
+    setColor $1
+    matchGAV $2
     tmpfile=`mktemp`
     curl -s -H "Content-Type: application/json" -X GET "$target/$color?groupid=${groupId}&artifactid=${artifactId}&version=${version}" > $tmpfile
     if grep -q '"contains":true' $tmpfile; then
@@ -73,26 +80,55 @@ check() {
     rm $tmpfile
 }
 
-if [ $# -lt 2 ]; then
-    printUsage
-    exit
-fi
+pom() {
+    mvn_opts=""
+    if [ "x$1" = "x--no-transitive" ]; then
+        mvn_opts="$mvn_opts -DexcludeTransitive=true"
+    fi
 
-action=$1
-color=$2
+    if [ -t 1 ]; then
+        RED="$(tput setaf 1)"
+        GREEN="$(tput setaf 2)"
+        YELLOW="$(tput setaf 3)"
+        DEFAULT="$(tput sgr0)"
+    fi
 
-case $color in
-    w) color=white ;;
-    b) color=black ;;
-    white|black) ;;
-    *) printUsage ;;
-esac
+    tmpfile=`mktemp`
+    mvn -q dependency:list -DoutputFile=$tmpfile -DappendOutput=true $mvn_opts
+    if [ $? -ne 0 ]; then
+        rm $tmpfile
+        exit
+    fi
 
-case $action in
-    add) add $3;;
-    delete) delete $3;;
-    check) check $3;;
-    list) list;;
-    *) printUsage ;;
-esac
-
+    sort -u $tmpfile | grep "^ *.*:.*:.*:.*"| sed "s/^ *//" | awk 'BEGIN {IFS=":"; FS=":"; OFS=":"} {print $1,$2,$4}' | while read line; do
+        wresp=`check white $line`
+        bresp=`check black $line`
+        if echo $wresp | grep -q "is whitelisted"; then
+            wl=true
+        elif echo $wresp | grep -q "is NOT whitelisted"; then
+            wl=false
+        else
+            echo "Error communicating with Black&White list service"
+            break
+        fi
+        if echo $bresp | grep -q "is blacklisted"; then
+            bl=true
+        elif echo $bresp | grep -q "is NOT blacklisted"; then
+            bl=false
+        else
+            echo "Error communicating with Black&White list service"
+            break
+        fi
+        if $wl && $bl; then
+            echo "${YELLOW}Both lists: $line"
+        elif $wl; then
+            echo "${GREEN}White list: $line"
+        elif $bl; then
+            echo "${RED}Black list: $line"
+        else
+            echo "${DEFAULT}None list:  $line"
+        fi
+    done
+    echo -n "$DEFAULT"
+    rm $tmpfile
+}
