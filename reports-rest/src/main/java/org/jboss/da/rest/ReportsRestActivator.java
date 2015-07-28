974e866c43795ea1d4ce0e6f90b638aa758620ac
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-07-27 23:35:51 -0400

    Activate Reports JAX-RS class

--- reports-rest/src/main/java/org/jboss/da/rest/ReportsRestActivator.java
+++ reports-rest/src/main/java/org/jboss/da/rest/ReportsRestActivator.java
@@ -1,30 +1,48 @@
 package org.jboss.da.rest;
 
 import org.jboss.da.rest.listings.Artifacts;
+import org.jboss.da.rest.reports.Reports;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 
+ *
  * @author Jozef Mrazek <jmrazek@redhat.com>
  *
  */
 @ApplicationPath("/rest")
 public class ReportsRestActivator extends Application {
 
-    private static Class<?>[] restClasses = new Class<?>[] { Root.class, Artifacts.class,
-    com.wordnik.swagger.jaxrs.listing.ApiListingResource.class,
-    com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider.class,
-    com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class,
-    com.wordnik.swagger.jaxrs.listing.ResourceListingProvider.class};
-
     @Override
     public Set<Class<?>> getClasses() {
-        return new HashSet<Class<?>>(Arrays.asList(restClasses));
+        Set<Class<?>> resources = new HashSet<>();
+        addSwaggerResources(resources);
+        addProjectResources(resources);
+        return resources;
+    }
+
+    /**
+     * Swagger classes required to generate the API JSON generation
+     * @param resources
+     */
+    public void addSwaggerResources(Set<Class<?>> resources) {
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+        resources.add(com.wordnik.swagger.jaxrs.listing.ResourceListingProvider.class);
+    }
+
+    /**
+     * Add all JAX-RS classes here to get activated!
+     * @param resources
+     */
+    public void addProjectResources(Set<Class<?>> resources) {
+        resources.add(Root.class);
+        resources.add(Artifacts.class);
+        resources.add(Reports.class);
     }
 }
