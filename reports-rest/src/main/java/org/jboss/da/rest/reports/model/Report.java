8e74f9406ddb51bf93b8f07fb36a9fc9f1769a8c
Jozef Mrazek <jmrazek@redhat.com>
2015-08-06 14:48:41 +0200

    Remove unsupported XmlElement anotations

--- reports-rest/src/main/java/org/jboss/da/rest/reports/model/Report.java
+++ reports-rest/src/main/java/org/jboss/da/rest/reports/model/Report.java
@@ -2,7 +2,6 @@ package org.jboss.da.rest.reports.model;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import java.util.List;
@@ -22,34 +21,27 @@ public class Report {
 
     @Getter
     @NonNull
-    @XmlElement(required = true, name = "group_id")
     private final String groupId;
 
     @Getter
     @NonNull
-    @XmlElement(required = true, name = "artifact_id")
     private final String artifactId;
 
     @Getter
     @NonNull
-    @XmlElement(required = true, name = "version")
     private final String version;
 
     @Getter
     @NonNull
-    @XmlElement(required = true, name = "available_versions")
     private final List<String> availableVersions;
 
     @Getter
-    @XmlElement(required = false, name = "best_match_version")
     private final String bestMatchVersion;
 
     @Getter
-    @XmlElement(required = false, name = "dependency_versions_satisfied")
     private final boolean dependencyVersionsSatisfied;
 
     @Getter
     @NonNull
-    @XmlElement(required = true, name = "dependencies")
     private final List<Report> dependencies;
 }
