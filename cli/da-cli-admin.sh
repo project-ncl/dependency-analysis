52a6f28d471a6eda24bc1f7b970b411fd1717474
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-08-12 10:28:53 -0400

    Some tiny improvements to the "Usage"
    1. Add new lines after each option
    2. Add the expected output for pom-report option

--- cli/da-cli-admin.sh
+++ cli/da-cli-admin.sh
@@ -6,22 +6,32 @@ listings="$basedir/listings.sh"
 . $listings
 
 printUsage() {
+    echo ""
     echo "$0 check (b[lack]|w[hite]) GROUP_ID:ARTIFACT_ID:VERSION"
     echo "    Check if artifact GROUP_ID:ARTIFACT_ID:VERSION is in black or white list"
+    echo ""
     echo "$0 add (b[lack]|w[hite]) GROUP_ID:ARTIFACT_ID:VERSION"
     echo "    Add artifact GROUP_ID:ARTIFACT_ID:VERSION to black or white list"
+    echo ""
     echo "$0 delete (b[lack]|w[hite]) GROUP_ID:ARTIFACT_ID:VERSION"
     echo "    Delete artifact GROUP_ID:ARTIFACT_ID:VERSION from black or white list"
+    echo ""
     echo "$0 list (b[lack]|w[hite])"
     echo "    List all artifacts in black or white list"
+    echo ""
     echo "$0 pom-bw [--no-transitive]";
     echo "    Check all dependencies from pom in working directory (using dependency:list) and print their Black/White list status"
+    echo ""
     echo "$0 pom-report [--no-transitive]";
     echo "    Check all dependencies from pom in working directory (using dependency:list) and print their report status"
+    echo "    Output: <groupId>:<artifactId>:<version> :: <groupId>:<artifactId>:<version> <Best Matched Red Hat Version> <Available Versions> <In black/white list?>"
+    echo ""
     echo "$0 lookup";
     echo "    Read G:A:Vs from standard input and finds corresponding redhat versions"
+    echo ""
     echo "$0 report GROUP_ID:ARTIFACT_ID:VERSION";
     echo "    Generate dependency report for GROUP_ID:ARTIFACT_ID:VERSION"
+    echo ""
     exit
 }
 
