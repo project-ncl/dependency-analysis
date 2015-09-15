7cad9b90c45e8dd5fefb30887cbf53c78d33ec66
Jozef Mrazek <jmrazek@redhat.com>
2015-09-15 09:48:36 +0200

    [DA-137] Wrong convert of community versions to OSGi one

--- common/src/main/java/org/jboss/da/common/version/OSGiVersionParser.java
+++ common/src/main/java/org/jboss/da/common/version/OSGiVersionParser.java
@@ -7,9 +7,24 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class OSGiVersionParser {
 
+    /**
+     * Converts version to osgi compliant
+     * 
+     * @param version
+     * @return
+     */
+    
     public String getOSGiVersion(String version) {
-        Version osgi = new Version(version);
-        return osgi.getOSGiVersionString();
+        Version osgi;
+        if (version.matches(".*[.-]redhat-(\\d+)")) {
+            osgi = new Version(version);
+            return osgi.getOSGiVersionString();
+        }
+        osgi = new Version(version + ".redhat");
+        String osgiS = osgi.getOSGiVersionString();
+        if (osgiS.endsWith(".redhat"))
+            return osgiS.replace(".redhat", "");
+        return osgiS.replace("-redhat", "");
     }
 
 }
