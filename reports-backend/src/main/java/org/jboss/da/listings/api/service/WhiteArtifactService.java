6773d51edae1ac5e5d2d0f88630fbe44ca394f34
Honza Brázdil <jbrazdil@redhat.com>
2015-09-15 11:19:41 +0200

    Rework B&W list so it is easier for user to use it
    In Whitelist we now allow only redhat versions of artifacts.
      If non-redhat version is being added, error 400 is returned.
      If redhat version is being checked, exact match is searched.
      If non-redhat version is being checked, it is converted to OSGi
      version and then all redhad version of this artifact are searched.
    
    In Blacklist we now store non-redhat OSGi versions of artifact.
      If redhat version is being added, the redhat suffix is stripped and
      then converted to OSGi.
      If non-redhat version is being added, it is converted to OSGi version.
      If redhat version is being checked, the redhat suffix is stripped and
      then converted to OSGi.
      If non-redhat version is being checked, it is converted to OSGi version.

--- reports-backend/src/main/java/org/jboss/da/listings/api/service/WhiteArtifactService.java
+++ reports-backend/src/main/java/org/jboss/da/listings/api/service/WhiteArtifactService.java
@@ -1,5 +1,7 @@
 package org.jboss.da.listings.api.service;
 
+import java.util.List;
+import org.jboss.da.communication.model.GAV;
 import org.jboss.da.listings.api.model.WhiteArtifact;
 
 /**
@@ -9,4 +11,30 @@ import org.jboss.da.listings.api.model.WhiteArtifact;
  */
 public interface WhiteArtifactService extends ArtifactService<WhiteArtifact> {
 
+    /**
+     * Add artifact to whitelist.
+     * The version must contain redhat suffix.
+     * @throws IllegalArgumentException when version doesn't have redhat suffix.
+     */
+    @Override
+    public STATUS addArtifact(String groupId, String artifactId, String version)
+            throws IllegalArgumentException;
+
+    /**
+     * Checks if whitelist contains artifact with specific groupId, artifactId and version.
+     * If the version have redhat suffix, find exact match. If the version doesn't have redhat 
+     * suffix, converts the version to OSGi version and finds any redhat suffixed versions
+     * in whitelist.
+     * @return List of found artifacts.
+     */
+    public List<WhiteArtifact> getArtifacts(GAV gav);
+
+    /**
+     * Checks if whitelist contains artifact with specific groupId, artifactId and version.
+     * If the version have redhat suffix, find exact match. If the version doesn't have redhat
+     * suffix, converts the version to OSGi version and finds any redhat suffixed versions
+     * in whitelist.
+     * @return List of found artifacts.
+     */
+    public List<WhiteArtifact> getArtifacts(String groupId, String artifactId, String version);
 }
--- reports-backend/src/main/java/org/jboss/da/listings/impl/dao/WhiteArtifactDAOImpl.java
+++ reports-backend/src/main/java/org/jboss/da/listings/impl/dao/WhiteArtifactDAOImpl.java
@@ -1,6 +1,12 @@
 package org.jboss.da.listings.impl.dao;
 
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.jboss.da.listings.api.dao.WhiteArtifactDAO;
 import org.jboss.da.listings.api.model.WhiteArtifact;
@@ -16,5 +22,22 @@ public class WhiteArtifactDAOImpl extends ArtifactDAOImpl<WhiteArtifact> impleme
 
     public WhiteArtifactDAOImpl() {
         super(WhiteArtifact.class);
+    }
+
+    @Override
+    public List<WhiteArtifact> findRedhatArtifact(String groupId, String artifactId, String version) {
+        String dotVersion = version + ".redhat-%";
+        String dashVersion = version + "-redhat-%";
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<WhiteArtifact> cq = cb.createQuery(type);
+        Root<WhiteArtifact> artifact = cq.from(type);
+        cq.select(artifact).where(
+                cb.and(cb.equal(artifact.get("artifactId"), artifactId),
+                        cb.equal(artifact.get("groupId"), groupId),
+                        cb.or(cb.like(artifact.get("version"), dotVersion),
+                                cb.like(artifact.get("version"), dashVersion))));
+        TypedQuery<WhiteArtifact> q = em.createQuery(cq);
+        return q.getResultList();
     }
 }
