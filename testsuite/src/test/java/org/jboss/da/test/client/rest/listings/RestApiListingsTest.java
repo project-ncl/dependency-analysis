f78ddf54d0a9c60f09fbb661856c00fb6c455f8e
Jozef Mrazek <jmrazek@redhat.com>
2015-09-15 16:00:15 +0200

    Base for B/W tests

---
+++ testsuite/src/test/java/org/jboss/da/test/client/rest/listings/RestApiListingsTest.java
@@ -0,0 +1,129 @@
+package org.jboss.da.test.client.rest.listings;
+
+import org.jboss.da.test.client.AbstractRestApiTest;
+import org.junit.Test;
+
+public class RestApiListingsTest extends AbstractRestApiTest {
+
+    @Test
+    public void testAddWhiteArtifact() {
+
+    }
+
+    @Test
+    public void testAddNonRHWhiteArtifact() {
+
+    }
+
+    @Test
+    public void testAddBlackArtifact() {
+
+    }
+
+    @Test
+    public void testAddBlackRHArtifact() {
+
+    }
+
+    @Test
+    public void testAddBlackNonOSGiArtifact() {
+
+    }
+
+    @Test
+    public void testAddBlacklistedArtifactToWhitelist() {
+
+    }
+
+    @Test
+    public void testAddWhitelistedArtifactToBlacklist() {
+
+    }
+
+    @Test
+    public void testAddMultipleTimeWhitelistedArtifactToBlacklist() {
+
+    }
+
+    @Test
+    public void testAlreadyAddedWhiteArtifact() {
+
+    }
+
+    @Test
+    public void testAlreadyAddedBlackArtifact() {
+
+    }
+
+    @Test
+    public void testDeleteWhiteArtifact() {
+
+    }
+
+    @Test
+    public void testDeleteBlackArtifact() {
+
+    }
+
+    @Test
+    public void testDeleteNonExistingWhiteArtifact() {
+
+    }
+
+    @Test
+    public void testDeleteNonExistingBlackArtifact() {
+
+    }
+
+    @Test
+    public void testCheckRHWhiteArtifact() {
+
+    }
+
+    /**
+     * Non RedHat but OSGi compliant white artifact test
+     */
+    @Test
+    public void testCheckNonRHWhiteArtifact() {
+
+    }
+
+    /**
+     * Non RedHat non OSGi compliant white artifact test
+     */
+    @Test
+    public void testCheckNonRHNonOSGiWhiteArtifact() {
+
+    }
+
+    @Test
+    public void testCheckRHBlackArtifact() {
+
+    }
+
+    /**
+     * Non RedHat but OSGi compliant black artifact test
+     */
+    @Test
+    public void testCheckNonRHBlackArtifact() {
+
+    }
+
+    /**
+     * Non RedHat non OSGi compliant black artifact test
+     */
+    @Test
+    public void testCheckNonRHNonOSGiBlackArtifact() {
+
+    }
+
+    @Test
+    public void testGetAllWhiteArtifacts() {
+
+    }
+
+    @Test
+    public void testGetAllBlackArtifacts() {
+
+    }
+}
