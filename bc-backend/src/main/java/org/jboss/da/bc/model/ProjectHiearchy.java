e1cc2627a32d973e5c0835e39cec51b17f7bf175
Honza Brázdil <jbrazdil@redhat.com>
2015-09-22 14:15:50 +0200

    Backend API

---
+++ bc-backend/src/main/java/org/jboss/da/bc/model/ProjectHiearchy.java
@@ -0,0 +1,40 @@
+package org.jboss.da.bc.model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.codehaus.jackson.annotate.JsonUnwrapped;
+
+/**
+ *
+ * @author Honza Brázdil <jbrazdil@redhat.com>
+ */
+@ToString
+public class ProjectHiearchy {
+
+    @Getter
+    @Setter
+    @JsonUnwrapped
+    private ProjectDetail project;
+
+    @Getter
+    @Setter
+    private boolean selected;
+
+    @Getter
+    private Optional<Set<ProjectHiearchy>> dependencies = Optional.of(Collections.emptySet());
+
+    public ProjectHiearchy(ProjectDetail project, boolean nextLevel) {
+        this.project = project;
+        this.selected = nextLevel;
+    }
+
+    public void setDependencies(Optional<Set<ProjectHiearchy>> dependencies) {
+        this.dependencies = dependencies;
+    }
+
+}
