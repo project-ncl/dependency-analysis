1f8913c89997550e12e61c566925ba0bda949920
Honza Brázdil <jbrazdil@redhat.com>
2015-10-14 18:48:06 +0200

    Fix finalizer so build configurations don't link to all transitive dependencies

--- bc-backend/src/main/java/org/jboss/da/bc/backend/impl/FinalizerImpl.java
+++ bc-backend/src/main/java/org/jboss/da/bc/backend/impl/FinalizerImpl.java
@@ -46,40 +46,42 @@ public class FinalizerImpl implements Finalizer {
     @Override
     public Integer createBCs(String name, String productVersion, ProjectHiearchy toplevelBc,
             String bcSetName) throws Exception {
-        Set<Integer> ids = create(toplevelBc);
+        Set<Integer> ids = new HashSet<>();
+        create(toplevelBc, ids);
         int productVersionId = bcSetGenerator.createProduct(name, productVersion);
         bcSetGenerator.createBCSet(bcSetName, productVersionId, new ArrayList<>(ids));
         return productVersionId;
     }
 
-    private Set<Integer> create(ProjectHiearchy hiearchy) throws Exception {
-        Set<Integer> deps = new HashSet<>();
-        if (hiearchy.isSelected()) {
+    private Integer create(ProjectHiearchy hiearchy, Set<Integer> allDependencyIds) throws Exception {
+        Set<Integer> nextLevelDependencyIds = new HashSet<>();
 
-            for (ProjectHiearchy dep : hiearchy.getDependencies().orElseGet(Collections::emptySet)) {
-                deps.addAll(create(dep));
+        for (ProjectHiearchy dep : hiearchy.getDependencies().orElseGet(Collections::emptySet)) {
+            if (dep.isSelected()) {
+                nextLevelDependencyIds.add(create(dep, allDependencyIds));
             }
+        }
 
-            ProjectDetail project = hiearchy.getProject();
-            BuildConfigurationCreate bcc;
-            if(project.isUseExistingBc()){
-                Optional<BuildConfiguration> bc = bcFinder.lookupBcByScm(project.getScmUrl(), project.getScmRevision());
-                bcc = bc.orElseThrow(() -> new IllegalStateException("useExistingBC is true, but there is no BC to use."));
-            }else{
-                bcc = toBC(project, deps);
-                if(project.isCloneRepo()){
-                    try{
-                        String newScmUrl = repoCloner.cloneRepository(project.getScmUrl(), project.getScmRevision(), SCMType.GIT, "Repository of " + project.getGav());
-                        bcc.setScmRepoURL(newScmUrl);
-                    }catch(Exception ex){
-                        log.error("Failed to clone repo.", ex);
-                    }
+        ProjectDetail project = hiearchy.getProject();
+        BuildConfigurationCreate bcc;
+        if (project.isUseExistingBc()) {
+            Optional<BuildConfiguration> bc = bcFinder.lookupBcByScm(project.getScmUrl(), project.getScmRevision());
+            bcc = bc.orElseThrow(() -> new IllegalStateException("useExistingBC is true, but there is no BC to use."));
+        } else {
+            bcc = toBC(project, nextLevelDependencyIds);
+            if (project.isCloneRepo()) {
+                try {
+                    String newScmUrl = repoCloner.cloneRepository(project.getScmUrl(), project.getScmRevision(), SCMType.GIT, "Repository of " + project.getGav());
+                    bcc.setScmRepoURL(newScmUrl);
+                } catch (Exception ex) {
+                    log.error("Failed to clone repo.", ex);
                 }
             }
-            BuildConfiguration bc = pnc.createBuildConfiguration(bcc);
-            deps.add(bc.getId());
         }
-        return deps;
+        BuildConfiguration bc = pnc.createBuildConfiguration(bcc);
+        allDependencyIds.add(bc.getId());
+
+        return bc.getId();
     }
 
     private BuildConfigurationCreate toBC(ProjectDetail project, Set<Integer> deps) {
