6c228e15f664a927553140b622547f0728ba2824
Dustin Kut Moy Cheung <dcheung@redhat.com>
2015-10-26 09:49:46 -0400

    [DA-158] Shallow clone git repositories when possible
    Otherwise fallback to the full git cloning.

--- source-code-manager/src/main/java/org/jboss/da/scm/impl/SCMClonner.java
+++ source-code-manager/src/main/java/org/jboss/da/scm/impl/SCMClonner.java
@@ -11,15 +11,22 @@ import org.apache.maven.scm.provider.git.jgit.JGitScmProvider;
 import org.apache.maven.scm.provider.svn.svnexe.SvnExeScmProvider;
 import org.apache.maven.scm.repository.ScmRepository;
 import org.apache.maven.scm.repository.ScmRepositoryException;
-import javax.enterprise.context.ApplicationScoped;
-import java.io.File;
 import org.apache.maven.scm.command.checkout.CheckOutScmResult;
+import org.slf4j.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * This code is mostly based from the SCM code in pnc-local, written by Ahmed Abu Lawi (@ahmedlawi92)
  */
 @ApplicationScoped
 public class SCMClonner {
+
+    @Inject
+    private Logger logger;
 
     private ScmManager scmManager;
 
@@ -38,6 +45,10 @@ public class SCMClonner {
             cloneTo.mkdir();
         }
 
+        if (shallowClone(scmType, scmUrl, revision, cloneTo)) {
+            return;
+        }
+        // if we can't shallow clone, then do the full git clone
         ScmRepository repo = getScmRepository(scmType.getSCMUrl(scmUrl), scmManager);
         CheckOutScmResult checkOut = scmManager.checkOut(repo, new ScmFileSet(cloneTo), new ScmTag(
                 revision));
@@ -54,6 +65,48 @@ public class SCMClonner {
             throw new ScmException("Could not find a provider.", ex);
         } catch (ScmRepositoryException ex) {
             throw new ScmException("Error while connecting to the repository", ex);
+        }
+    }
+
+    /**
+     * If we were able to shallow clone, this function will return true.
+     * false otherwise
+     *
+     * @param scmType type of scmUrl
+     * @param scmUrl link of repo to clone
+     * @param revision specific revision to clone
+     * @param cloneTo directory to clone the scmUrl
+     * @return
+     */
+    private boolean shallowClone(SCMType scmType, String scmUrl, String revision, File cloneTo) {
+
+        // we only support git for shallow cloning
+        if (!scmType.equals(SCMType.GIT)) {
+            return false;
+        }
+
+        try {
+            ProcessBuilder pb;
+            if (revision == null || revision.isEmpty()) {
+                pb = new ProcessBuilder("git", "clone", "--depth", "1", scmUrl, ".");
+            } else {
+                pb = new ProcessBuilder("git", "clone", "--depth", "1", "--branch", revision,
+                        scmUrl, ".");
+            }
+            pb.directory(cloneTo);
+
+            Map<String, String> env = pb.environment();
+
+            // need to add those variables to tell git not to prompt us if repository does not exist
+            env.put("GIT_ASKPASS", "/bin/echo"); // git <= 2.3
+            env.put("GIT_TERMINAL_PROMPT", "0"); // git > 2.3
+
+            Process p = pb.start();
+            int status = p.waitFor();
+            return status == 0;
+        } catch (IOException | InterruptedException ex) {
+            logger.error("Could not shallow clone", ex);
+            return false;
         }
     }
 }
