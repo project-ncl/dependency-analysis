484481eaf03874813fdd317bd1b4c85ac77d9c0f
Honza Brázdil <jbrazdil@redhat.com>
2015-11-11 16:13:08 +0100

    [DA-179] bump versions of galley and cartographer
    Galley in version 0.10.3 fixed issue with NPE.
    We had to bump also version of cartographer and add galley-cdi-embedder
    so there are no missing or ambiguous dependencies.

--- communication/src/main/java/org/jboss/da/communication/CartographerProducer.java
+++ communication/src/main/java/org/jboss/da/communication/CartographerProducer.java
@@ -7,22 +7,10 @@ import org.commonjava.cartographer.spi.graph.discover.DiscoverySourceManager;
 import org.commonjava.maven.atlas.graph.RelationshipGraphFactory;
 import org.commonjava.maven.atlas.graph.spi.neo4j.FileNeo4jConnectionFactory;
 import org.commonjava.maven.galley.cache.FileCacheProviderConfig;
-import org.commonjava.maven.galley.maven.ArtifactManager;
-import org.commonjava.maven.galley.maven.parse.MavenPomReader;
-import org.commonjava.maven.galley.spi.auth.PasswordManager;
-import org.commonjava.maven.galley.spi.cache.CacheProvider;
-import org.commonjava.maven.galley.spi.event.FileEventManager;
-import org.commonjava.maven.galley.spi.io.PathGenerator;
-import org.commonjava.maven.galley.spi.io.TransferDecorator;
-import org.commonjava.maven.galley.spi.nfc.NotFoundCache;
-import org.commonjava.maven.galley.spi.transport.LocationExpander;
-import org.commonjava.maven.galley.spi.transport.LocationResolver;
-import org.commonjava.maven.galley.transport.htcli.Http;
 import org.jboss.da.communication.pom.qualifier.DACartographerCore;
 import javax.enterprise.inject.Produces;
 import java.io.File;
 import java.io.IOException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Class used to inject null to '@Injects' in cartographer
@@ -41,27 +29,7 @@ public class CartographerProducer {
     }
 
     @Produces
-    public Http getHttp() {
-        return null;
-    }
-
-    @Produces
-    public LocationExpander getLocationExpander() {
-        return null;
-    }
-
-    @Produces
-    public ArtifactManager getArtifactManager() {
-        return null;
-    }
-
-    @Produces
     public DiscoverySourceManager getDiscoverySourceManager() {
-        return null;
-    }
-
-    @Produces
-    public LocationResolver getLocationResolver() {
         return null;
     }
 
@@ -71,47 +39,8 @@ public class CartographerProducer {
     }
 
     @Produces
-    public MavenPomReader getMavenPomReader() {
-        return null;
-    }
-
-    @Produces
     public FileCacheProviderConfig getFileCacheProviderConfig() {
-        return null;
-    }
-
-    @Produces
-    public NotFoundCache getNotFoundCache() {
-        return null;
-    }
-
-    @Produces
-    public CacheProvider getCacheProvider() {
-        return null;
-    }
-
-    @Produces
-    public TransferDecorator getTransferDecorator() {
-        return null;
-    }
-
-    @Produces
-    public ObjectMapper getObjectMapper() {
-        return null;
-    }
-
-    @Produces
-    public PathGenerator getPathGenerator() {
-        return null;
-    }
-
-    @Produces
-    public FileEventManager getFileEventManager() {
-        return null;
-    }
-
-    @Produces
-    public PasswordManager getPasswordManager() {
-        return null;
+        FileCacheProviderConfig fcpc = new FileCacheProviderConfig(new File("random"));
+        return fcpc;
     }
 }
