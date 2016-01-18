12cbbfafac0cb61beeb0eeb374ee19fe309f7730
Jozef Mrazek <jmrazek@redhat.com>
2016-01-18 12:28:18 +0100

    [DA-228] Remove hardcoded application version, add property loading from file

---
+++ common/src/main/templates/org/jboss/da/common/Constants.java
@@ -0,0 +1,11 @@
+package org.jboss.da.common;
+
+public final class Constants {
+
+    public static final String DA_VERSION = "@version.da@";
+
+    public static final String REST_API_VERSION_BC = "@version.bc.rest@";
+
+    public static final String REST_API_VERSION_REPORTS = "@version.reports.rest@";
+
+}
--- testsuite/src/test/java/org/jboss/da/test/client/rest/RestApiIndexTest.java
+++ testsuite/src/test/java/org/jboss/da/test/client/rest/RestApiIndexTest.java
@@ -6,10 +6,10 @@ import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
 import org.junit.Test;
 
-import java.io.File;
-
-import static org.apache.commons.io.FileUtils.readFileToString;
 import static org.apache.http.entity.ContentType.TEXT_HTML;
+import static org.jboss.da.common.Constants.DA_VERSION;
+import static org.jboss.da.common.Constants.REST_API_VERSION_BC;
+import static org.jboss.da.common.Constants.REST_API_VERSION_REPORTS;
 import static org.junit.Assert.assertEquals;
 
 public class RestApiIndexTest extends AbstractRestReportsTest {
@@ -23,9 +23,27 @@ public class RestApiIndexTest extends AbstractRestReportsTest {
 
         ClientResponse<String> response = request.get(String.class);
 
-        File expectedResponseFile = new ExpectedResponseFilenameBuilder(
-                restApiExpectedResponseFolder, path, contentType).getFile();
-        assertEquals(readFileToString(expectedResponseFile), response.getEntity(String.class));
+        assertEquals(getExpectedResponse(), response.getEntity(String.class));
+    }
+
+    private String getExpectedResponse() {
+        return "<h1>Dependency analysis service REST API</h1>"
+                + "\n"
+                + "<ul><li><strong>DA Version:</strong> "
+                + DA_VERSION
+                + "</li>"
+                + "\n"
+                + "<li><strong>BC REST API Version:</strong> "
+                + REST_API_VERSION_BC
+                + "</li>"
+                + "\n"
+                + "<li><strong>Reports REST API Version:</strong> "
+                + REST_API_VERSION_REPORTS
+                + "</li>"
+                + "\n"
+                + "<li><a href=\"../doc\">Swagger documentation</a></li>"
+                + "\n"
+                + "<li><strong>REST proposal documentation:</strong> <a href=\"https://docs.engineering.redhat.com/display/JP/REST+endpoints+proposal\">https://docs.engineering.redhat.com/display/JP/REST+endpoints+proposal</a></li></ul>";
     }
 
 }
