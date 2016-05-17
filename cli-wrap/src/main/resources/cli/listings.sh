91e802e9f7a13307bb5f7a16047fa64162e342c4
Jozef Mrazek <jmrazek@redhat.com>
2016-05-17 18:12:46 +0200

    [DA-261] Add version number to the CLI

---
+++ cli-wrap/src/main/resources/cli/listings.sh
@@ -0,0 +1,353 @@
+#!/bin/bash
+DA_MAIN_SERVER="ncl-test-vm-01.host.prod.eng.bos.redhat.com:8180/da/rest/v-0.4"
+# DA_TEST_SERVER is defined in the da-cli-test.sh script
+target="${DA_TEST_SERVER:-${DA_MAIN_SERVER}}"
+
+basedir=`dirname $0`
+
+setColor() {
+    case $1 in
+        w) color=white ;;
+        b) color=black ;;
+        white|black) color=$1;;
+        *) printUsage; exit ;;
+    esac
+}
+
+prettyPrintGAV() {
+    python -m json.tool | \
+        egrep '"(artifactId|groupId|version)"' | \
+        sed -r 's/ *"groupId": "([^"]*)",?/g:\1\t/;
+                s/ *"artifactId": "([^"]*)",?/a:\1\t/;
+                s/ *"version": "([^"]*)",?/v:\1\t~/;' | \
+        tr -d "\n" | tr "~" "\n" | \
+        sed -r 's/(g:([^\t]*)\t|a:([^\t]*)\t|v:([^\t]*)\t)*/\2:\3:\4/'
+}
+
+prettyPrint() {
+    pushd "${basedir}" > /dev/null
+    python -c "import pretty; pretty.$1()"
+    popd > /dev/null
+}
+
+gettmpfile() {
+    mktemp 2>/dev/null || mktemp -t 'pncl'
+}
+
+get() {
+    curl -s -H "Content-Type: application/json" -X GET "$target/$1"
+}
+
+post() {
+    curl -s -H "Content-Type: application/json" -X POST -d "$2" "$target/$1"
+}
+
+delete() {
+    case $1 in
+        white)
+            if [ -z "$3" ]; then
+                ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} delete --white $2
+            else
+                ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} delete --white $2 --product $3
+            fi
+            ;;
+        black) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} delete --black $2;;
+        whitelist-product) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} delete --whitelist-product $2;;
+        *) echo "I don't know this option"
+    esac
+}
+
+matchGAV() {
+    [ -z $1 ] && printUsage
+    if ! echo $1 | grep -q "^[^:]\+:[^:]\+:[^:]\+$"; then
+        echo "the GAV is not in format GROUP_ID:ARTIFACT_ID:VERSION: $1"
+        exit 1
+    fi
+    groupId=`echo $1 | cut -f1 -d:`
+    artifactId=`echo $1 | cut -f2 -d:`
+    version=`echo $1 | cut -f3 -d:`
+}
+
+formatGAVjson() {
+    echo '{"groupId":"'${groupId}'", "artifactId":"'${artifactId}'", "version":"'${version}'"'$1'}'
+}
+
+list() {
+
+    case $1 in
+        white) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} list --white $2;;
+        black) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} list --black;;
+        whitelist-products) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} list --whitelist-products $2;;
+        whitelist-ga) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} list --whitelist-ga $2 $3;;
+        whitelist-gav) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} list --whitelist-gav $2 $3;;
+        whitelist-gavs) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} list --whitelist-gavs $2;;
+        *) echo "I don't know this option"
+    esac
+}
+
+add() {
+    case $1 in
+        white) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} add --white $2 $3;;
+        black) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} add --black $2;;
+        whitelist-product) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} add --whitelist-product $2 $3;;
+        *) echo "I don't know this option"
+    esac
+
+}
+
+update() {
+    case $1 in
+        whitelist-product) ${basedir}/da-cli-script.py --server http://${DA_MAIN_SERVER} update --whitelist-product $2 $3;;
+        *) echo "I don't know this option"
+    esac
+}
+
+check() {
+    setColor $1
+    matchGAV $2
+    tmpfile=`gettmpfile`
+    get "listings/${color}list/gav?groupid=${groupId}&artifactid=${artifactId}&version=${version}" > $tmpfile
+    if grep -q '"contains":true' $tmpfile; then
+        echo -n "Artifact $groupId:$artifactId:$version is ${color}listed - actual verisions in list: "
+        cat $tmpfile | prettyPrint listCheck
+    elif grep -q '"contains":false' $tmpfile; then
+        echo "Artifact $groupId:$artifactId:$version is NOT ${color}listed"
+    else
+        echo "Error checking $groupId:$artifactId:$version"
+        cat $tmpfile
+        echo
+    fi
+    rm $tmpfile
+}
+
+report() {
+    type="pretty"
+    if [ "X$1" = "X--raw" ]; then
+        type="raw"
+        shift
+    elif [ "X$1" = "X--json" ]; then
+        type="json"
+        shift
+    fi
+    matchGAV $1
+    tmpfile=`gettmpfile`
+    post "reports/gav" "`formatGAVjson`" >> $tmpfile
+    case $type in
+        pretty) cat $tmpfile | prettyPrint report | column -t -s $'\t' ;;
+        raw) cat $tmpfile | prettyPrint reportRaw ;;
+        json) cat $tmpfile ;;
+    esac
+    rm $tmpfile
+}
+
+lookup() {
+    local query="["
+    if [ $# -ge 1 ]; then # G:A:V specified on command line
+        matchGAV $1
+        query="$query `formatGAVjson`"
+    else                  # G:A:Vs specified in standart input
+        local first=true
+        while read line; do
+            matchGAV $line
+            $first || query="$query,"
+            first=false
+            query="$query `formatGAVjson`"
+        done
+    fi
+    query="$query ]"
+    tmpfile=`gettmpfile`
+    post "reports/lookup/gavs" "$query" > $tmpfile
+    cat $tmpfile | prettyPrint lookup
+    rm $tmpfile
+}
+
+parse_pom_bw_report_options() {
+
+    pom_transitive_flag=false;
+
+    local wrong_option=""
+
+    case $1 in
+        --transitive) pom_transitive_flag=true;;
+        -*)           wrong_option="${key}";;
+        *)            pom_path="${key}";;
+    esac
+
+    # if wrong flag passed
+    if ! [ -z "${wrong_option}" ]; then
+        echo ""
+        echo "Wrong option: '${wrong_option}' specified. Aborting"
+        exit 1
+    fi
+
+    if [ ${pom_transitive_flag} == true ]; then
+        pom_path="$2"
+        prod_version="$3"
+    else
+        pom_path="$1"
+        prod_version="$2"
+    fi
+
+    # if path does not exist
+    if ! [ -d "${pom_path}" ]; then
+        echo ""
+        echo "The directory '${pom_path}' does not exist! Aborting"
+        exit 1
+    fi
+
+    # if path does not contain the pom.xml file
+    if ! [ -f "${pom_path}/pom.xml" ]; then
+        echo ""
+        echo "No pom.xml file present in the directory '${pom_path}'. Aborting"
+        exit 1
+    fi
+}
+
+pom_bw_junit_xml() {
+    parse_pom_bw_report_options "$@"
+
+    mvn_opts=""
+    if [ ${pom_transitive_flag} = true ]; then
+        mvn_opts="$mvn_opts"
+    else
+        mvn_opts="$mvn_opts -DexcludeTransitive=true"
+    fi
+
+    local tmpfile=`gettmpfile`
+    pushd "${pom_path}" > /dev/null
+    mvn -q dependency:list -DoutputFile=$tmpfile -DappendOutput=true $mvn_opts
+
+    if [ $? -ne 0 ]; then
+        rm $tmpfile
+        echo ""
+        echo ""
+        echo "================================================================="
+        echo "'mvn dependency:list' command failed."
+        echo "Consider running 'mvn clean install' before running the pom-bw command again to fix the issue"
+        echo "================================================================="
+        exit
+    fi
+
+    popd > /dev/null
+
+    local pkg_list_file=`gettmpfile`
+    sort -u $tmpfile | grep "^ *.*:.*:.*:.*"| sed "s/^ *//" | awk 'BEGIN {IFS=":"; FS=":"; OFS=":"} {print $1,$2,$4}' | while read line; do
+        echo "${line}" >> ${pkg_list_file}
+    done
+
+    python ${basedir}/testsuite.py ${pkg_list_file} ${prod_version}
+
+    rm ${tmpfile}
+    rm ${pkg_list_file}
+}
+
+scm_report() {
+    local type="pretty"
+    if [ "X$1" = "X--raw" ]; then
+        type="raw"
+        shift
+    elif [ "X$1" = "X--json" ]; then
+        type="json"
+        shift
+    fi
+
+    local scm="$1"
+    local tag="$2"
+    local pom_path="$3"
+
+    if [ -z "${scm}" ] || [ -z "${tag}" ] || [ -z "${pom_path}" ]; then
+        echo "Error: You have to specify the scm, scm tag and the pom path to analyze"
+        exit 1
+    fi
+    local scmJSON="{\"scmUrl\": \"${scm}\", \"revision\": \"${tag}\", \"pomPath\": \"${pom_path}\"}"
+
+    local report="$(post "reports/scm" "${scmJSON}")"
+    case $type in
+      pretty) echo "$report" | prettyPrint report | column -t -s $'\t' ;;
+      raw) echo "$report" | prettyPrint reportRaw ;;
+      json) echo "$report" ;;
+    esac
+}
+
+scm_report_adv() {
+    local type="pretty"
+    if [ "X$1" = "X--json" ]; then
+        type="json"
+        shift
+    fi
+
+    local scm="$1"
+    local tag="$2"
+    local pom_path="$3"
+
+    if [ -z "$scm" ] || [ -z "$tag" ] || [ -z "$pom_path" ]; then
+        echo "Error: You have to specify the scm, scm tag and the pom path to analyze"
+        exit 1
+    fi
+    local scmJSON="{\"scmUrl\": \"${scm}\", \"revision\": \"${tag}\", \"pomPath\": \"${pom_path}\"}"
+
+    tmpfile=`gettmpfile`
+    post "reports/scm-advanced" "$scmJSON" >> $tmpfile
+    case $type in
+        pretty) cat $tmpfile | prettyPrint reportAdvSum | column -t -s $'\t'
+                echo
+                cat $tmpfile | prettyPrint reportAdv | column -t -s $'\t'
+            ;;
+        json) cat $tmpfile ;;
+    esac
+    rm $tmpfile
+}
+
+scm_report_align() {
+    local type="pretty"
+    local unknown="false"
+    local products=""
+    local repo=""
+
+    while [ $# -gt 0 ]; do
+        case $1 in
+            --json) type="json" ;;
+            --unknown) unknown="true" ;;
+            --products)
+                [ -n "$products" ] && products="$products,"
+                products="$products$2"
+                shift
+                ;;
+            --repository)
+                [ -n "$repo" ] && repo="$repo,"
+                repo="$repo\"$2\""
+                shift
+                ;;
+             *) break ;;
+        esac
+        shift
+    done
+
+    if [ $# -lt 2 ]; then
+        echo "You must specify SCM and TAG"
+        exit
+    fi
+
+    local scm="$1"
+    local tag="$2"
+    local pomPath="$3"
+
+    local query="{"
+    query="$query \"scmUrl\": \"$scm\""
+    query="$query, \"revision\": \"$tag\""
+    query="$query, \"pomPath\": \"$pomPath\""
+    query="$query, \"searchUnknownProducts\": $unknown"
+    query="$query, \"products\": [$products]"
+    query="$query, \"additionalRepos\": [$repo]"
+    query="$query }"
+
+    tmpfile=`gettmpfile`
+    post "reports/align" "$query" >> $tmpfile
+    case $type in
+        pretty) cat $tmpfile | prettyPrint reportAlign | column -t -s $'\t' ;;
+        json) cat $tmpfile ;;
+    esac
+    rm $tmpfile
+}
+
+
---
+++ cli-wrap/src/main/resources/cli/pretty.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python
+import json
+import sys
+import fileinput
+
+
+def formatProduct(product, supportStatus=True):
+    if supportStatus:
+        return product["name"] + ":" + product["version"] + " " + product["supportStatus"]
+    else:
+        return product["name"] + ":" + product["version"]
+
+def formatVersionProduct(vp):
+    return vp['version'] + " (" + formatProduct(vp['product'], False) + ")" 
+
+def getBestSupportStatus(products):
+    status = "UNKNOWN"
+    for product in products:
+        ps = product["supportStatus"]
+        if (  (status == "UNKNOWN" and ps in {"UNSUPPORTED", "SUPERSEDED", "SUPPORTED"})
+           or (status == "UNSUPPORTED" and ps in {"SUPERSEDED", "SUPPORTED"})
+           or (status == "SUPERSEDED" and ps in {"SUPPORTED"}) ):
+            status = product["supportStatus"]
+    return status
+
+def getList(artifact, raw):
+    if artifact["blacklisted"] and artifact["whitelisted"]:
+        return "both lists"
+    elif artifact["blacklisted"]:
+        return "blacklisted"
+    elif artifact["whitelisted"]:
+        products = artifact["whitelisted"]
+        if raw:
+            return ",".join(map(formatProduct, products))
+        else:
+            return "whitelisted (" + str(len(products)) + ", " + getBestSupportStatus(products) + ")"
+    else:
+        return "graylisted"
+
+def getAvailableVersions(artifact, raw):
+    if "availableVersions" in artifact:
+        if raw:
+            return ",".join(artifact["availableVersions"])
+        else:
+            return str(len(artifact["availableVersions"])) + " available versions"
+    else:
+        return ""
+
+def getGAV(artifact):
+    return artifact["groupId"]+":"+artifact["artifactId"]+":"+artifact["version"]
+
+def getGAVList(artifacts):
+    return ", ".join(map(getGAV,artifacts))
+
+def getDependencyVersionsSatisfied(artifact):
+    if artifact["dependencyVersionsSatisfied"]:
+        return "all dependencies built"
+    else:
+        return str(artifact["notBuiltDependencies"]) + " dependencies not built"
+
+def printReport(artifact, depth = None):
+    lists = getList(artifact, depth is None)
+    versions = getAvailableVersions(artifact, depth is None)
+    gav = getGAV(artifact)
+    bestMatch = str(artifact["bestMatchVersion"])
+    depsSatisfied = getDependencyVersionsSatisfied(artifact)
+
+    if depth is None:
+        indent = ""
+    else:
+        indent = "| " * (depth -1)
+        if depth > 0:
+            indent += "+-"
+
+    print indent + gav + "\t" + bestMatch + "\t" + lists + "\t" + depsSatisfied +  "\t" + versions
+
+    for dep in artifact["dependencies"]:
+        if depth is None:
+            printReport(dep)
+        else:
+            printReport(dep, depth + 1)
+
+def printReportAdvSum(data):
+    print "Blacklisted artifacts:\t" + getGAVList(data["blacklistedArtifacts"])
+
+    print "Whitelisted artifacts:"
+    for whitelist in data["whitelistedArtifacts"]:
+        print "  " + getGAV(whitelist) + "\t" + ", ".join(map(formatProduct, whitelist["products"]))
+
+    print "Built community artifacts:"
+    for bestMatch in data["communityGavsWithBestMatchVersions"]:
+        print "  " + getGAV(bestMatch) + "\t" + bestMatch["bestMatchVersion"]
+
+    print "Community artifacts with other built version:"
+    for builtVersion in data["communityGavsWithBuiltVersions"]:
+        print "  " + getGAV(builtVersion) + "\t" +  ", ".join(builtVersion["availableVersions"])
+
+    print "Community artifacts:\t" + getGAVList(data["communityGavs"])
+
+def printReportAlign(data):
+    print "Internaly built:"
+    for module in data['internallyBuilt']:
+        print "  " + module['groupId'] + ":" + module['artifactId']
+        for dependency in module['gavProducts']:
+            print "    " + getGAV(dependency) + " - " + ", ".join(map(formatVersionProduct,dependency['gavProducts']))
+    print
+
+    print "Built in different version:"
+    for module in data['builtInDifferentVersion']:
+        print "  " + module['groupId'] + ":" + module['artifactId']
+        for dependency in module['gavProducts']:
+            print "    " + getGAV(dependency) + " - " + ", ".join(map(formatVersionProduct,dependency['gavProducts']))
+    print
+
+    print "Not built:"
+    for module in data['notBuilt']:
+        print "  " + module['groupId'] + ":" + module['artifactId']
+        for dependency in module['gavs']:
+            print "    " + getGAV(dependency)
+    print
+
+    print "Blacklisted:"
+    for module in data['blacklisted']:
+        print "  " + module['groupId'] + ":" + module['artifactId']
+        for dependency in module['gavs']:
+            print "    " + getGAV(dependency)
+
+def printLookup(artifact):
+    gav = getGAV(artifact)
+    bestMatch = str(artifact["bestMatchVersion"])
+    lists = getList(artifact, True)
+    versions = getAvailableVersions(artifact, True)
+
+    print gav + "\t" + bestMatch + "\t" + lists + "\t" + versions
+
+def readInput():
+    data=""
+    for line in fileinput.input():
+        data += line
+    return json.loads(data)
+
+def checkError(data):
+    if "message" in data:
+        # message key present when there is an error
+        print(data["message"])
+        sys.exit(1)
+
+def report():
+    data = readInput()
+    checkError(data)
+    print "tree of GAVs\tBest Match Version\tBlack/White list\t# of not built dependencies\t# of available versions"
+    printReport(data,0)
+
+def reportRaw():
+    data = readInput()
+    checkError(data)
+    printReport(data)
+
+def reportAdv():
+    data = readInput()
+    checkError(data)
+    print "tree of GAVs\tBest Match Version\tBlack/White list\t# of not built dependencies\t# of available versions"
+    printReport(data["report"], 0)
+    
+def reportAdvSum():
+    data = readInput()
+    checkError(data)
+    printReportAdvSum(data)
+
+def reportAlign():
+    data = readInput()
+    checkError(data)
+    printReportAlign(data)
+    
+def lookup():
+    data = readInput()
+    checkError(data)
+    for artifact in data:
+        printLookup(artifact)
+
+def listCheck():
+    data = readInput()
+    versions = []
+    for gav in data["found"]:
+        versions.append(gav["version"])
+    print ", ".join(versions)
+
+def hello():
+    print "hi"
