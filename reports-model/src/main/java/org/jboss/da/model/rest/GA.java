af0efed476dc248a6c06e3b23f16cb0eca9aa7ae
Honza Brázdil <jbrazdil@redhat.com>
2016-12-01 12:42:28 +0100

    Relax GAV validation, allow dash in group id
    Although maven specification prohibits it, maven doesn't enfore it.
    That means considerable amount of project with dash in groupId exists.

--- reports-model/src/main/java/org/jboss/da/model/rest/GA.java
+++ reports-model/src/main/java/org/jboss/da/model/rest/GA.java
@@ -13,7 +13,7 @@ import lombok.NonNull;
 public class GA implements Comparable<GA> {
 
     private static final Pattern GROUP_ID_PATTERN = Pattern
-            .compile("([a-zA-Z_][a-zA-Z\\d_]*\\.)*[a-zA-Z_][a-zA-Z\\d_]*");
+            .compile("([a-zA-Z_][a-zA-Z\\d_-]*\\.)*[a-zA-Z_][a-zA-Z\\d_-]*");
 
     private static final Pattern ARTIFACT_ID_PATTERN = Pattern.compile("[a-zA-Z0-9_.-]+");
 
