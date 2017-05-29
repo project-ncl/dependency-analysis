3022f8d31aadd43cdef9a700988b834a46ccff70
Honza Brázdil <jbrazdil@redhat.com>
2017-05-29 16:48:24 +0200

    Extract blacklist rest endpoints to solo class with interface

---
+++ reports-rest/src/main/java/org/jboss/da/rest/listings/BlackListImpl.java
@@ -0,0 +1,95 @@
+package org.jboss.da.rest.listings;
+
+import org.jboss.da.listings.api.model.BlackArtifact;
+import org.jboss.da.listings.api.service.ArtifactService;
+import org.jboss.da.listings.api.service.BlackArtifactService;
+import org.jboss.da.listings.model.rest.ContainsResponse;
+import org.jboss.da.listings.model.rest.RestArtifact;
+import org.jboss.da.listings.model.rest.SuccessResponse;
+import org.jboss.da.model.rest.ErrorMessage;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.jboss.da.rest.spi.BlackList;
+
+/**
+ *
+ * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
+ */
+public class BlackListImpl implements BlackList {
+
+    @Inject
+    private RestConvert convert;
+
+    @Inject
+    private BlackArtifactService blackService;
+
+    @Override
+    public Collection<RestArtifact> getAllBlackArtifacts() {
+        List<RestArtifact> artifacts = new ArrayList<>();
+        artifacts.addAll(convert.toRestArtifacts(blackService.getAll()));
+        return artifacts;
+    }
+
+    @Override
+    public Response isBlackArtifactPresent(String groupId, String artifactId, String version) {
+        if (groupId == null || artifactId == null || version == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(new ErrorMessage(ErrorMessage.eType.PARAMS_REQUIRED, "All parameters are required")).build();
+        }
+        ContainsResponse response = new ContainsResponse();
+
+        Optional<BlackArtifact> artifact = blackService.getArtifact(groupId, artifactId, version);
+        List<BlackArtifact> artifacts = artifact
+                .map(Collections::singletonList)
+                .orElse(Collections.emptyList());
+
+        response.setContains(artifact.isPresent());
+        response.setFound(convert.toRestArtifacts(artifacts));
+
+        if (artifact.isPresent()) {
+            return Response.ok(response).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).entity(response).build();
+        }
+    }
+
+    @Override
+    public Response addBlackArtifact(RestArtifact artifact) {
+        SuccessResponse response = new SuccessResponse();
+        ArtifactService.ArtifactStatus result = blackService.addArtifact(artifact.getGroupId(),
+                artifact.getArtifactId(), artifact.getVersion());
+        switch (result) {
+            case ADDED:
+                response.setSuccess(true);
+                return Response.ok(response).build();
+            case WAS_WHITELISTED:
+                response.setSuccess(true);
+                response.setMessage("Artifact was moved from whitelist into blacklist");
+                return Response.ok(response).build();
+            case NOT_MODIFIED:
+                response.setSuccess(false);
+                return Response.ok(response).build();
+            default:
+                return Response
+                        .status(Response.Status.INTERNAL_SERVER_ERROR)
+                        .entity(new ErrorMessage(ErrorMessage.eType.UNEXPECTED_SERVER_ERR,
+                                "Unexpected server error occurred.")).build();
+        }
+    }
+
+    @Override
+    public SuccessResponse removeBlackArtifact(RestArtifact artifact) {
+        SuccessResponse response = new SuccessResponse();
+        response.setSuccess(blackService.removeArtifact(artifact.getGroupId(),
+                artifact.getArtifactId(), artifact.getVersion()));
+        return response;
+    }
+}
