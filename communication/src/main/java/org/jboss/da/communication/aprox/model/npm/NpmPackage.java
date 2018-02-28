ecce94158548cd5059a59fb8f5ac91ca8a7bb763
Honza Brázdil <jbrazdil@redhat.com>
2018-02-28 13:52:06 +0100

    [NCL-3623] Get list of available versions for NPM artifacts from Indy

---
+++ communication/src/main/java/org/jboss/da/communication/aprox/model/npm/NpmPackage.java
@@ -0,0 +1,15 @@
+package org.jboss.da.communication.aprox.model.npm;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
+ */
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NpmPackage {
+
+}
