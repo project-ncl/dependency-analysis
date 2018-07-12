032f5fea3aefc39b12a29b5f0f99a7b9ae6d1917
Honza Brázdil <jbrazdil@redhat.com>
2018-07-12 20:14:06 +0200

    Workaround broken serialization of JSONRPC in websockets

--- testsuite/src/test/java/org/jboss/da/test/client/ws/reports/WebsocketApiReportsTest.java
+++ testsuite/src/test/java/org/jboss/da/test/client/ws/reports/WebsocketApiReportsTest.java
@@ -18,6 +18,10 @@ public class WebsocketApiReportsTest extends AbstractWebsocketReportsTest {
 
     private static final String METHOD_REPORTS_BUILT = "reports.built";
 
+    private static final String PATH_REPORTS_LOOKUP = "/reports/lookup/gavs";
+
+    private static final String METHOD_REPORTS_LOOKUP = "reports.lookup.gav";
+
     @Test
     public void testAlignReportBasic() throws IOException, Exception {
         assertResponseForRequest(PATH_REPORTS_ALIGN, "dependency-analysis", METHOD_REPORTS_ALIGN);
@@ -26,6 +30,11 @@ public class WebsocketApiReportsTest extends AbstractWebsocketReportsTest {
     @Test
     public void testBuiltReportBasic() throws IOException, Exception {
         assertResponseForRequest(PATH_REPORTS_BUILT, "dependency-analysis", METHOD_REPORTS_BUILT);
+    }
+
+    @Test
+    public void testLookupReportBasic() throws IOException, Exception {
+        assertResponseForRequest(PATH_REPORTS_LOOKUP, "guava13", METHOD_REPORTS_LOOKUP);
     }
 
     @Override
