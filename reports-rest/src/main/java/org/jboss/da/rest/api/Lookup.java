40a82659ef778ce6605c7c04ba21e339db8abbfd
Andrea Vibelli <avibelli@redhat.com>
2022-11-16 16:49:22 +0100

    [NCL-7335] Move instrumentation of attributes to the implementation classes

--- reports-rest/src/main/java/org/jboss/da/rest/api/Lookup.java
+++ reports-rest/src/main/java/org/jboss/da/rest/api/Lookup.java
@@ -7,8 +7,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import io.opentelemetry.instrumentation.annotations.SpanAttribute;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -26,7 +24,6 @@ import org.jboss.da.lookup.model.NPMLookupRequest;
 import org.jboss.da.lookup.model.NPMLookupResult;
 import org.jboss.da.lookup.model.NPMVersionsRequest;
 import org.jboss.da.lookup.model.NPMVersionsResult;
-import org.jboss.da.reports.model.response.NPMVersionsReport;
 
 /**
  *
@@ -42,17 +39,13 @@ public interface Lookup {
     @Path(value = "/maven")
     @Operation(summary = "Finds best matching versions for given Maven artifact coordinates (GAV).")
     @ApiResponse(content = @Content(array = @ArraySchema(schema = @Schema(implementation = MavenLookupResult.class))))
-    @WithSpan()
-    Set<MavenLookupResult> lookupMaven(@SpanAttribute(value = "request") MavenLookupRequest request)
-            throws CommunicationException;
+    Set<MavenLookupResult> lookupMaven(MavenLookupRequest request) throws CommunicationException;
 
     @POST
     @Path(value = "/maven/versions")
     @Operation(summary = "Lookup and filter available versions for the given Maven artifact coordinates (GAV).")
     @ApiResponse(content = @Content(array = @ArraySchema(schema = @Schema(implementation = MavenLookupResult.class))))
-    @WithSpan()
-    Set<MavenVersionsResult> versionsMaven(@SpanAttribute(value = "request") MavenVersionsRequest request)
-            throws CommunicationException;
+    Set<MavenVersionsResult> versionsMaven(MavenVersionsRequest request) throws CommunicationException;
 
     @POST
     @Path(value = "/maven/latest")
@@ -60,24 +53,18 @@ public interface Lookup {
             summary = "Finds latest matching versions for given Maven artifact coordinates (GAV), including bad versions.",
             description = "This endpoint is used for version increment so it will search all possible places and qualities of artifacts, including deleted and blocklisted artifacts.")
     @ApiResponse(content = @Content(array = @ArraySchema(schema = @Schema(implementation = MavenLatestResult.class))))
-    @WithSpan()
-    Set<MavenLatestResult> lookupMaven(@SpanAttribute(value = "request") MavenLatestRequest request)
-            throws CommunicationException;
+    Set<MavenLatestResult> lookupMaven(MavenLatestRequest request) throws CommunicationException;
 
     @POST
     @Path(value = "/npm")
     @Operation(summary = "Finds best matching versions for given NPM artifact coordinates (name, version).")
     @ApiResponse(content = @Content(array = @ArraySchema(schema = @Schema(implementation = NPMLookupResult.class))))
-    @WithSpan()
-    Set<NPMLookupResult> lookupNPM(@SpanAttribute(value = "request") NPMLookupRequest request)
-            throws CommunicationException;
+    Set<NPMLookupResult> lookupNPM(NPMLookupRequest request) throws CommunicationException;
 
     @POST
     @Path(value = "/npm/versions")
     @Operation(summary = "Lookup and filter available versions for the given NPM artifact coordinates (name, version).")
     @ApiResponse(content = @Content(array = @ArraySchema(schema = @Schema(implementation = NPMVersionsResult.class))))
-    @WithSpan()
-    Set<NPMVersionsResult> versionsNPM(@SpanAttribute(value = "request") NPMVersionsRequest request)
-            throws CommunicationException;
+    Set<NPMVersionsResult> versionsNPM(NPMVersionsRequest request) throws CommunicationException;
 
 }
