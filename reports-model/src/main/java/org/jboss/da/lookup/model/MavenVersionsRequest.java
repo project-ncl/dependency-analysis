d913d8e9341c604ca026d475d445a271165e8992
Honza Brázdil <jbrazdil@redhat.com>
2023-01-10 15:28:48 +0100

    Add @Valid to /lookup endpoints

--- reports-model/src/main/java/org/jboss/da/lookup/model/MavenVersionsRequest.java
+++ reports-model/src/main/java/org/jboss/da/lookup/model/MavenVersionsRequest.java
@@ -16,7 +16,6 @@
 package org.jboss.da.lookup.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
