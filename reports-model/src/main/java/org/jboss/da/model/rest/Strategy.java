a40c278efe76e3fb89c8056eb56f455abd90dc40
jmichalo <jmichalo@redhat.com>
2023-05-04 22:04:14 +0200

    [Alignment 2.0] changes after review

--- reports-model/src/main/java/org/jboss/da/model/rest/Strategy.java
+++ reports-model/src/main/java/org/jboss/da/model/rest/Strategy.java
@@ -1,20 +1,14 @@
 package org.jboss.da.model.rest;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
 @Builder
 @Jacksonized
-@ToString
-@EqualsAndHashCode
+@Value
 public class Strategy {
 
     private final String artifactScope;
