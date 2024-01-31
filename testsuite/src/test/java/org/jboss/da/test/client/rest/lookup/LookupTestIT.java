986d878a6f07483326b256baa3556aaecffc632f
Honza Brázdil <jbrazdil@redhat.com>
2024-01-31 13:18:23 +0100

    Fix double in

--- testsuite/src/test/java/org/jboss/da/test/client/rest/lookup/LookupTestIT.java
+++ testsuite/src/test/java/org/jboss/da/test/client/rest/lookup/LookupTestIT.java
@@ -1,15 +1,17 @@
 package org.jboss.da.test.client.rest.lookup;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
 import org.jboss.da.test.client.rest.AbstractRestReportsTest;
 import org.junit.Test;
 
+import static org.jboss.da.test.client.rest.listings.AbstractRestApiListingTest.PATH_BLACK_LISTINGS_GAV;
 import static org.junit.Assert.assertEquals;
 
 public class LookupTestIT extends AbstractRestReportsTest {
 
-    private static final String PATH_LOOKUP_MAVEN = "/lookup/maven";
+    static final String PATH_LOOKUP_MAVEN = "/lookup/maven";
     private static final String PATH_LATEST_MAVEN = "/lookup/maven/latest";
     private static final String PATH_MAVEN_VERSIONS = "/lookup/maven/versions";
 
