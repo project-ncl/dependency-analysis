dc9273f26a7d2270577658129cd9ea74ec7d06d9
Jozef Mrazek <jmrazek@redhat.com>
2025-08-05 13:06:59 +0200

    Create super-classes for lookup results

--- reports-model/src/main/java/org/jboss/da/lookup/model/NPMVersionsResult.java
+++ reports-model/src/main/java/org/jboss/da/lookup/model/NPMVersionsResult.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.jboss.da.model.rest.NPMPackage;
@@ -29,16 +30,17 @@ import java.util.List;
  *
  * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class NPMVersionsResult {
-
-    @NonNull
-    @JsonUnwrapped
-    private NPMPackage npmPackage;
+public class NPMVersionsResult extends NPMResult {
 
     @NonNull
     private List<String> availableVersions;
+
+    public NPMVersionsResult(@NonNull NPMPackage npmPackage, @NonNull List<String> availableVersions) {
+        super(npmPackage);
+        this.availableVersions = availableVersions;
+    }
 }
