2e19eef7a6ee7167790e41034eeabbbed01cca8f
Nick Cross <ncross@redhat.com>
2026-03-24 13:03:34 +0000

    NCL-9564 Inline compareVersions

--- reports-model/src/main/java/org/jboss/da/model/rest/GAV.java
+++ reports-model/src/main/java/org/jboss/da/model/rest/GAV.java
@@ -1,6 +1,8 @@
 package org.jboss.da.model.rest;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -17,6 +19,8 @@ import lombok.NonNull;
 @JsonPropertyOrder({ "groupId", "artifactId", "version" })
 public class GAV implements Comparable<GAV> {
 
+    private static final Pattern pattern = Pattern.compile(
+            "^(?<major>[0-9]{1,9})?(\\.(?<minor>[0-9]{1,9})(\\.(?<micro>[0-9]{1,9}))?)?([.-]?(?<qualifier>.+?))?$");
     @NonNull
     private final GA ga;
 
@@ -31,6 +35,57 @@ public class GAV implements Comparable<GAV> {
             @JsonProperty("version") String version) {
         this.ga = new GA(groupId, artifactId);
         this.version = Objects.requireNonNull(version);
+    }
+
+    public static int compareVersions(String version1, String version2) {
+        Matcher matcher1 = pattern.matcher(version1);
+        Matcher matcher2 = pattern.matcher(version2);
+        if (!matcher1.matches() || !matcher2.matches()) {
+            throw new IllegalArgumentException("Version " + version1 + " or " + version2 + " is unparsable");
+        }
+        int major1 = parseNumberString(matcher1.group("major"));
+        int minor1 = parseNumberString(matcher1.group("minor"));
+        int micro1 = parseNumberString(matcher1.group("micro"));
+        String qualifier1 = matcher1.group("qualifier") == null ? "" : matcher1.group("qualifier");
+
+        int major2 = parseNumberString(matcher2.group("major"));
+        int minor2 = parseNumberString(matcher2.group("minor"));
+        int micro2 = parseNumberString(matcher2.group("micro"));
+        String qualifier2 = matcher2.group("qualifier") == null ? "" : matcher2.group("qualifier");
+
+        int r = Integer.compare(major1, major2);
+        if (r != 0) {
+            return r;
+        }
+        r = Integer.compare(minor1, minor2);
+        if (r != 0) {
+            return r;
+        }
+        r = Integer.compare(micro1, micro2);
+        if (r != 0) {
+            return r;
+        }
+
+        String[] tokens1 = qualifier1.split("[.-]");
+        String[] tokens2 = qualifier2.split("[.-]");
+
+        for (int i = 0; i < tokens1.length && i < tokens2.length; i++) {
+            try {
+                int num1 = Integer.parseInt(tokens1[i]);
+                int num2 = Integer.parseInt(tokens2[i]);
+                r = Integer.compare(num1, num2);
+            } catch (NumberFormatException ex) {
+                r = tokens1[i].compareTo(tokens2[i]);
+            }
+            if (r != 0) {
+                return r;
+            }
+        }
+        return tokens1.length - tokens2.length;
+    }
+
+    private static int parseNumberString(String segmentString) {
+        return segmentString == null ? 0 : Integer.parseInt(segmentString);
     }
 
     @JsonIgnore
@@ -55,7 +110,7 @@ public class GAV implements Comparable<GAV> {
     public int compareTo(GAV o) {
         int gaCmp = this.ga.compareTo(o.ga);
         if (gaCmp == 0) {
-            return DummyVersionComparator.compareVersions(this.version, o.version);
+            return compareVersions(this.version, o.version);
         } else {
             return gaCmp;
         }
--- reports-model/src/test/java/org/jboss/da/model/rest/VersionComparatorTest.java
+++ reports-model/src/test/java/org/jboss/da/model/rest/VersionComparatorTest.java
@@ -13,35 +13,32 @@ public class VersionComparatorTest {
 
     @Test
     public void testCompareVersion() {
-        DummyVersionComparator vc = new DummyVersionComparator();
 
-        assertTrue(vc.compare("3.4.2.Final", "2.4.2.Final") > 0);
-        assertTrue(vc.compare("3.4.2.Final", "3.3.2.Final") > 0);
-        assertTrue(vc.compare("3.4.2.Final", "3.4.1.Final") > 0);
-        assertTrue(vc.compare("3.4.2.Final", "3.4.2.Beta") > 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "2.4.2.Final") > 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "3.3.2.Final") > 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "3.4.1.Final") > 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "3.4.2.Beta") > 0);
 
-        assertEquals(0, vc.compare("3.0.0.Final", "3.Final"));
-        assertEquals(0, vc.compare("3.0.0.Final", "3.0.Final"));
-        // noinspection EqualsWithItself
-        assertEquals(0, vc.compare("3.0.0.Final", "3.0.0.Final"));
+        assertEquals(0, GAV.compareVersions("3.0.0.Final", "3.Final"));
+        assertEquals(0, GAV.compareVersions("3.0.0.Final", "3.0.Final"));
+        assertEquals(0, GAV.compareVersions("3.0.0.Final", "3.0.0.Final"));
 
-        assertEquals(0, vc.compare("3.0.0", "3"));
-        assertEquals(0, vc.compare("3.0.0", "3.0"));
-        // noinspection EqualsWithItself
-        assertEquals(0, vc.compare("3.0.0", "3.0.0"));
+        assertEquals(0, GAV.compareVersions("3.0.0", "3"));
+        assertEquals(0, GAV.compareVersions("3.0.0", "3.0"));
+        assertEquals(0, GAV.compareVersions("3.0.0", "3.0.0"));
 
-        assertEquals(0, vc.compare("b10", "0.0.0.b10"));
+        assertEquals(0, GAV.compareVersions("b10", "0.0.0.b10"));
 
-        assertTrue(vc.compare("3.4.2.Final", "4.4.2.Final") < 0);
-        assertTrue(vc.compare("3.4.2.Final", "3.5.2.Final") < 0);
-        assertTrue(vc.compare("3.4.2.Final", "3.4.3.Final") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "4.4.2.Final") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "3.5.2.Final") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "3.4.3.Final") < 0);
 
-        assertTrue(vc.compare("3.4.2.Alpha", "3.4.2.Beta") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.Alpha", "3.4.2.Beta") < 0);
 
-        assertTrue(vc.compare("3.4.2.Final", "3.4.2.Final-redhat-1") < 0);
-        assertTrue(vc.compare("3.4.2", "3.4.2.redhat-1") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final", "3.4.2.Final-redhat-1") < 0);
+        assertTrue(GAV.compareVersions("3.4.2", "3.4.2.redhat-1") < 0);
 
-        assertTrue(vc.compare("3.4.2.Final-redhat-1", "3.4.2.Final-redhat-2") < 0);
-        assertTrue(vc.compare("3.4.2.redhat-1", "3.4.2.redhat-2") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.Final-redhat-1", "3.4.2.Final-redhat-2") < 0);
+        assertTrue(GAV.compareVersions("3.4.2.redhat-1", "3.4.2.redhat-2") < 0);
     }
 }
