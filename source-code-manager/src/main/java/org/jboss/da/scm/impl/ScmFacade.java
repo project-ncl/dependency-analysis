d35bea4756e4b1f66d8d4c7a5670a7a33fb16dd5
Patrik Korytar <patrik.korytar@gmail.com>
2026-06-30 13:44:55 +0200

    NCL-9724 Remove scm provider git implementation

--- source-code-manager/src/main/java/org/jboss/da/scm/impl/ScmFacade.java
+++ source-code-manager/src/main/java/org/jboss/da/scm/impl/ScmFacade.java
@@ -2,21 +2,21 @@ package org.jboss.da.scm.impl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.scm.ScmException;
 import org.apache.maven.scm.ScmFileSet;
 import org.apache.maven.scm.ScmTag;
-import org.apache.maven.scm.command.add.AddScmResult;
-import org.apache.maven.scm.command.checkin.CheckInScmResult;
 import org.apache.maven.scm.command.checkout.CheckOutScmResult;
 import org.apache.maven.scm.manager.BasicScmManager;
 import org.apache.maven.scm.manager.NoSuchScmProviderException;
 import org.apache.maven.scm.manager.ScmManager;
-import org.apache.maven.scm.provider.git.jgit.JGitScmProvider;
 import org.apache.maven.scm.provider.svn.svnexe.SvnExeScmProvider;
 import org.apache.maven.scm.repository.ScmRepository;
 import org.apache.maven.scm.repository.ScmRepositoryException;
@@ -38,13 +38,14 @@ public class ScmFacade {
     public ScmFacade() {
         scmManager = new BasicScmManager();
 
-        // Add new providers here
-        scmManager.setScmProvider(SCMType.GIT.toString(), new JGitScmProvider());
+        // git is handled via the git command (see gitCliClone)
+        // add new (non-git) providers here
         scmManager.setScmProvider(SCMType.SVN.toString(), new SvnExeScmProvider());
     }
 
     /**
      * Tries to do a shallow clone (clone only the requested revision) of the remote repository to the local directory.
+     * First tries to do it using the git, otherwise falls back to ScmManager.
      * If it is not possible to do that, then it does the full clone.
      *
      * @param scmType Type of the repository
@@ -59,7 +60,11 @@ public class ScmFacade {
             cloneTo.mkdir();
         }
 
-        if (shallowClone(scmType, scmUrl, revision, cloneTo)) {
+        if (scmType == SCMType.GIT) {
+            if (!gitCliClone(scmType, scmUrl, revision, cloneTo)) {
+                throw new ScmException(
+                        "Cloning of git repository " + scmUrl + " at revision " + revision + " failed.");
+            }
             return;
         }
 
@@ -76,41 +81,11 @@ public class ScmFacade {
      * @throws ScmException Thrown if the clone of the repository fails
      */
     public void cloneRepository(SCMType scmType, String scmUrl, String revision, File cloneTo) throws ScmException {
-        // if we can't shallow clone, then do the full git clone
         ScmRepository repo = getScmRepository(scmType.getSCMUrl(scmUrl), scmManager);
         CheckOutScmResult checkOut = scmManager.checkOut(repo, new ScmFileSet(cloneTo), new ScmTag(revision));
         if (!checkOut.isSuccess()) {
             throw new ScmException("Repository was not cloned: " + checkOut.getProviderMessage());
         }
-    }
-
-    /**
-     * Stages selected local files in the SCM repository and pushes them to the remote repository.
-     *
-     * @param scmType Type of the repository
-     * @param scmUrl URL to the repository
-     * @param baseDir Directory of the local repository
-     * @param files Files, which should be pushed to the remote
-     * @param commitMessage Commit message
-     * @throws ScmException Thrown if the operation with the repository fails
-     */
-    public void commitAndPush(SCMType scmType, String scmUrl, File baseDir, List<File> files, String commitMessage)
-            throws ScmException {
-        ScmRepository repo = getScmRepository(scmType.getSCMUrl(scmUrl), scmManager);
-        ScmFileSet scmFileSet = new ScmFileSet(baseDir, files);
-        AddScmResult addResult = scmManager.add(repo, scmFileSet);
-
-        if (!addResult.isSuccess())
-            throw new ScmException(
-                    "The manager wasn't able to ADD these files " + scmFileSet.toString() + " to the repository "
-                            + repo);
-
-        CheckInScmResult pushResult = scmManager.checkIn(repo, scmFileSet, commitMessage);
-
-        if (!pushResult.isSuccess())
-            throw new ScmException(
-                    "The manager wasn't able to PUSH these files " + scmFileSet.toString() + " to the repository "
-                            + repo);
     }
 
     private ScmRepository getScmRepository(String scmUrl, ScmManager scmManager) throws ScmException {
@@ -124,7 +99,9 @@ public class ScmFacade {
     }
 
     /**
-     * If we were able to shallow clone, this function will return true. false otherwise
+     * Clones the repository using the git. A branch or tag is cloned shallowly.
+     * Commit hash cannot be cloned shallowly and falls back to a full clone.
+     * Returns true if the clone succeeded, false otherwise.
      *
      * @param scmType type of scmUrl
      * @param scmUrl link of repo to clone
@@ -132,33 +109,60 @@ public class ScmFacade {
      * @param cloneTo directory to clone the scmUrl
      * @return
      */
-    private boolean shallowClone(SCMType scmType, String scmUrl, String revision, File cloneTo) {
-        // we only support git for shallow cloning
-        if (!scmType.equals(SCMType.GIT)) {
-            return false;
+    private boolean gitCliClone(SCMType scmType, String scmUrl, String revision, File cloneTo) {
+        if (revision == null || revision.isEmpty()) {
+            return runGit(cloneTo, "clone", "--depth", "1", scmUrl, ".");
         }
 
+        // works when the revision is a branch or tag name
+        if (runGit(cloneTo, "clone", "--depth", "1", "--branch", revision, scmUrl, ".")) {
+            return true;
+        }
+
+        cleanDirectory(cloneTo);
+
+        // revision is most likely a commit hash (cannot be cloned shallowly)
+        if (runGit(cloneTo, "clone", scmUrl, ".") && runGit(cloneTo, "checkout", revision)) {
+            return true;
+        }
+
+        cleanDirectory(cloneTo);
+        return false;
+    }
+
+    private boolean runGit(File workingDir, String... args) {
+        List<String> command = new ArrayList<>(args.length + 1);
+        command.add("git");
+        Collections.addAll(command, args);
+
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.directory(workingDir);
+        pb.redirectOutput(ProcessBuilder.Redirect.DISCARD);
+        pb.redirectError(ProcessBuilder.Redirect.DISCARD);
+
+        Map<String, String> env = pb.environment();
+
+        // need to add those variables to tell git not to prompt us if repository does not exist
+        env.put("GIT_ASKPASS", "/bin/echo"); // git <= 2.3
+        env.put("GIT_TERMINAL_PROMPT", "0"); // git > 2.3
+
         try {
-            ProcessBuilder pb;
-            if (revision == null || revision.isEmpty()) {
-                pb = new ProcessBuilder("git", "clone", "--depth", "1", scmUrl, ".");
-            } else {
-                pb = new ProcessBuilder("git", "clone", "--depth", "1", "--branch", revision, scmUrl, ".");
-            }
-            pb.directory(cloneTo);
-
-            Map<String, String> env = pb.environment();
-
-            // need to add those variables to tell git not to prompt us if repository does not exist
-            env.put("GIT_ASKPASS", "/bin/echo"); // git <= 2.3
-            env.put("GIT_TERMINAL_PROMPT", "0"); // git > 2.3
-
-            Process p = pb.start();
-            int status = p.waitFor();
-            return status == 0;
-        } catch (IOException | InterruptedException ex) {
-            logger.error("Could not shallow clone", ex);
+            return pb.start().waitFor() == 0;
+        } catch (IOException ex) {
+            logger.error("Could not run git {}", String.join(" ", args), ex);
             return false;
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            logger.error("Interrupted while running git {}", String.join(" ", args), ex);
+            return false;
+        }
+    }
+
+    private void cleanDirectory(File directory) {
+        try {
+            FileUtils.cleanDirectory(directory);
+        } catch (IOException ex) {
+            logger.warn("Could not clean directory {}", directory, ex);
         }
     }
 }
